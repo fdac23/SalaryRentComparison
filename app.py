a4d686f41b7f6474d9070546ebc05739e9e34a96
zperry4 <zactperry73@gmail.com>
2023-10-26 12:06:51 -0400

    Setup pog (data, files, etc)

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from dash import Dash, html
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
 
 app = Dash(__name__)
 
+### RANDOM STUFF TO GET STARTED
 app.layout = html.Div([
-    html.Div(children='Hello World')
+    dcc.RadioItems(
+        id='candidate', 
+        options=["Joly", "Coderre", "Bergeron"],
+        value="Coderre",
+        inline=True
+    ),
+    dcc.Graph(id="graph"),
 ])
+
+
+@app.callback(
+    Output("graph", "figure"), 
+    Input("candidate", "value"))
+def display_choropleth(candidate):
+    df = px.data.election() # replace with your own data source
+    geojson = px.data.election_geojson()
+    fig = px.choropleth(
+        df, geojson=geojson, color=candidate,
+        locations="district", featureidkey="properties.district",
+        projection="mercator", range_color=[0, 6500])
+    fig.update_geos(fitbounds="locations", visible=False)
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    return fig
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,2 @@
+# TODO: Clean up all data (for salaries, drop the zip code at end of city name, etc)
+# Add to consistency of data for both sets / general data processing
