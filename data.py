50254322678a0527de80ee687762aad43de82cc2
zperry4 <zactperry73@gmail.com>
2023-11-28 18:02:55 -0500

    Add rent trend plotting & some styling changes

--- data.py
+++ data.py
@@ -101,6 +101,43 @@ def compare_cities_median_wage(name1, wage1, name2, wage2):
     )
 
     fig = go.Figure(data=[trace], layout=layout)
+    fig.update_layout(height=400, width=500)
+
+    return dcc.Graph(figure=fig)
+
+  
+"""
+Compare the monthly rent between two selected cities.
+
+Args:
+    name1 (str): Name of the first city
+    rent1 (str): Monthly rent of the first city
+    name2 (str): Name of the second city
+    rent2 (str): Monthly rent of the second city
+
+Returns:
+    dcc.Graph: A bar graph comparing the monthly rent of the two cities for easy comparison
+"""
+def compare_cities_rent(name1, rent1, name2, rent2):
+    areas = [name2, name1]
+    rent1 = float(rent1)
+    rent2 = float(rent2)
+    rents = [rent2, rent1]
+    
+    trace = go.Bar(
+        x=areas,
+        y=rents,
+        marker=dict(color='green')  # Change color as needed
+    )
+
+    layout = go.Layout(
+        title='Monthly Rent Comparison',
+        xaxis=dict(title='City'),
+        yaxis=dict(title='Monthly Rent')
+    )
+
+    fig = go.Figure(data=[trace], layout=layout)
+    fig.update_layout(height=400, width=500)
 
     return dcc.Graph(figure=fig)
 
@@ -138,41 +175,7 @@ def compare_cities_ratio(name1, wage1, rent1, name2, wage2, rent2):
     )
 
     fig = go.Figure(data=[trace], layout=layout)
-
-    return dcc.Graph(figure=fig)
-
-  
-"""
-Compare the monthly rent between two selected cities.
-
-Args:
-    name1 (str): Name of the first city
-    rent1 (str): Monthly rent of the first city
-    name2 (str): Name of the second city
-    rent2 (str): Monthly rent of the second city
-
-Returns:
-    dcc.Graph: A bar graph comparing the monthly rent of the two cities for easy comparison
-"""
-def compare_cities_rent(name1, rent1, name2, rent2):
-    areas = [name2, name1]
-    rent1 = float(rent1)
-    rent2 = float(rent2)
-    rents = [rent2, rent1]
-    
-    trace = go.Bar(
-        x=areas,
-        y=rents,
-        marker=dict(color='green')  # Change color as needed
-    )
-
-    layout = go.Layout(
-        title='Monthly Rent Comparison',
-        xaxis=dict(title='City'),
-        yaxis=dict(title='Monthly Rent')
-    )
-
-    fig = go.Figure(data=[trace], layout=layout)
+    fig.update_layout(height=400, width=500)
 
     return dcc.Graph(figure=fig)
 
@@ -240,3 +243,72 @@ def extract_text_data(text):
         return name, wage, rent, _10th_percentile, _25th_percentile, _75th_percentile, _90th_percentile
     
     return "No matches found..."
+
+
+"""
+Plots the trends of rental pricing for the two selected cities. Also plots the average rental pricing across the US.
+
+Args:
+    data1 (dict): Dictionary containing the data for the first city
+    data2 (dict): Dictionary containing the data for the second city
+    wanted_rent_columns (list): List of the columns to extract from the rental data
+
+Returns:
+    dcc.Graph: Graph containing the rental trends for the two cities and the average rental trend across the US
+"""
+def rent_trend_plot(data1, data2, wanted_rent_columns):
+    city_1_name = list(data1.values())[0] + ", " + list(data1.values())[1]
+    city_2_name = list(data2.values())[0] + ", " + list(data2.values())[1]
+    plot_name = "Monthly Rent Comparison: " + city_1_name + " vs. " + city_2_name
+    
+    average_rent_trend = get_rent_trend_across_us(wanted_rent_columns)
+    
+    city1_plot = go.Scatter(
+        x=list(data1.keys())[2:],
+        y=list(data1.values())[2:],
+        mode='lines+markers',
+        name=city_1_name,
+    )
+    
+    city2_plot = go.Scatter(
+        x=list(data2.keys())[2:],
+        y=list(data2.values())[2:],
+        mode='lines+markers',
+        name=city_2_name,
+    )
+    
+    avg_plot = go.Scatter(
+        x=list(average_rent_trend.keys()),
+        y=list(average_rent_trend.values()),
+        mode='lines+markers',
+        name="Average Rent Across US",
+    )
+    
+    fig = go.Figure(data=[city1_plot, city2_plot, avg_plot])
+    
+    fig.update_layout(
+        title=plot_name,
+        xaxis_title="Date",
+        yaxis_title="Monthly Rent ($)",
+    )
+    
+    return dcc.Graph(figure=fig)
+
+
+"""
+Get the average rent trend across the US.
+
+Args:
+    wanted_rent_columns (list): List of the columns to extract from the rental data
+
+Returns:
+    dict: Dictionary containing the average rent trend across the US from 2015-2022
+"""
+def get_rent_trend_across_us(wanted_rent_columns):
+    df = pd.read_csv("./data/clean_data.csv")
+    
+    rent_dates_data = wanted_rent_columns[2:]
+    df_all_rent_dates = df[rent_dates_data]
+    average_rent_trend = df_all_rent_dates.mean()
+    
+    return average_rent_trend.to_dict()
--- map.py
+++ map.py
@@ -1,5 +1,5 @@
 from dash import Dash, dcc, html, Input, Output
-from data import compare_cities_median_wage, compare_cities_ratio, compare_cities_rent, compare_cities_percentiles, extract_text_data
+from data import compare_cities_median_wage, compare_cities_ratio, compare_cities_rent, compare_cities_percentiles, extract_text_data, rent_trend_plot
 from urllib.request import urlopen
 
 import plotly.express as px
@@ -18,18 +18,39 @@ def create_map():
 
     # df = pd.read_csv('./data/simplemaps_uscities_basicv1/uscities.csv')
     df = pd.read_csv('./data/clean_data.csv')
+  
+    wanted_rent_columns = [
+        'city', 'state_id',
+        '2015-01-31', '2015-02-28', '2015-03-31', '2015-04-30', '2015-05-31', '2015-06-30', '2015-07-31', '2015-08-31', '2015-09-30', '2015-10-31', '2015-11-30', '2015-12-31',
+        '2016-01-31', '2016-02-29', '2016-03-31', '2016-04-30', '2016-05-31', '2016-06-30', '2016-07-31', '2016-08-31', '2016-09-30', '2016-10-31', '2016-11-30', '2016-12-31',
+        '2017-01-31', '2017-02-28', '2017-03-31', '2017-04-30', '2017-05-31', '2017-06-30', '2017-07-31', '2017-08-31', '2017-09-30', '2017-10-31', '2017-11-30', '2017-12-31',
+        '2018-01-31', '2018-02-28', '2018-03-31', '2018-04-30', '2018-05-31', '2018-06-30', '2018-07-31', '2018-08-31', '2018-09-30', '2018-10-31', '2018-11-30', '2018-12-31',
+        '2019-01-31', '2019-02-28', '2019-03-31', '2019-04-30', '2019-05-31', '2019-06-30', '2019-07-31', '2019-08-31', '2019-09-30', '2019-10-31', '2019-11-30', '2019-12-31',
+        '2020-01-31', '2020-02-29', '2020-03-31', '2020-04-30', '2020-05-31', '2020-06-30', '2020-07-31', '2020-08-31', '2020-09-30', '2020-10-31', '2020-11-30', '2020-12-31',
+        '2021-01-31', '2021-02-28', '2021-03-31', '2021-04-30', '2021-05-31', '2021-06-30', '2021-07-31', '2021-08-31', '2021-09-30', '2021-10-31', '2021-11-30', '2021-12-31',
+        '2022-01-31', '2022-02-28', '2022-03-31', '2022-04-30', '2022-05-31'
+    ]
 
     city_selection = [None, None]
 
     # Create "text" column as string of information
     df['text'] = df['city'] + ', ' + df['state_id'] + ',' + (df['Annual median wage(2)']).astype(str) + ',' + round(df['2022-05-31'], 2).astype(str) + ',' + (df['Annual 10th percentile wage(2)']).astype(str) + ',' + (df['Annual 25th percentile wage(2)']).astype(str) + ',' + (df['Annual 75th percentile wage(2)']).astype(str) + ',' + (df['Annual 90th percentile wage(2)']).astype(str)
     df['hover_text'] = df['city'] + ', ' + df['state_id'] + '<br>' + 'Rent/Salary Ratio: ' + round((df['2022-05-31']*12/df['Annual median wage(2)'])*100, 3).astype(str) + '%'
-
+    
+    # Getting all rent data up to May 2022 for each city. Stores the Key with its corresponding value to be passed as customdata.
+    rent_data = []
+    for i, row in df.iterrows():
+        rent_data_dict = {}
+        for col_name in wanted_rent_columns:
+            rent_data_dict[col_name] = row[col_name]
+        rent_data.append(rent_data_dict)
+    
     fig = go.Figure(data=go.Scattergeo(
         lon = df['lng'],
         lat = df['lat'],
         text = df['text'],
         hovertext=df['hover_text'],
+        customdata=rent_data,
         mode = 'markers',
         marker_size = df['2022-05-31']*12/df['Annual median wage(2)']*100,
         marker_color = df['2022-05-31']*12/df['Annual median wage(2)']*100,
@@ -110,16 +131,19 @@ def create_map():
         wage_plot = compare_cities_median_wage(name1, wage1, name2, wage2)
         rent_plot = compare_cities_rent(name1, rent1, name2, rent2)
         ratio_plot = compare_cities_ratio(name1, wage1, rent1, name2, wage2, rent2)
-        percentiles_plot = compare_cities_percentiles(name1, wage1, _10th_percentile1, _25th_percentile1, _75th_percentile1, _90th_percentile1, name2, wage2, _10th_percentile2, _25th_percentile2, _75th_percentile2, _90th_percentile2)
+        
+        percentiles_plot = compare_cities_percentiles(name1, wage1, _10th_percentile1, _25th_percentile1, _75th_percentile1, _90th_percentile1, name2, wage2, _10th_percentile2, _25th_percentile2, _75th_percentile2, _90th_percentile2)    
+        rent_price_trend_plot = rent_trend_plot(city_selection[0]["points"][0]["customdata"], city_selection[1]["points"][0]["customdata"], wanted_rent_columns)
         
         return[
-            html.Div(style={'display': 'flex', 'flex-direction': 'row', 'justify-content': 'space-evenly'}, children = [
+            html.Div(style={'display': 'flex', 'flex-direction': 'row', 'justify-content': 'center'}, children = [
                 wage_plot,
                 rent_plot,
+                ratio_plot,
             ]),
             html.Div(style={'display': 'flex', 'flex-direction': 'row', 'justify-content': 'center'}, children = [
-                ratio_plot,
                 percentiles_plot,
+                rent_price_trend_plot,
             ])
         ]
         
